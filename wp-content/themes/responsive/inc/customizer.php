b60064076419415db6e8ea52adcafd002c964770
Doan Van Loi <loidoan@smart.net.vn>
2014-10-21 16:09:32 +0700

    sidebar button

--- wp-content/themes/responsive/inc/customizer.php
+++ wp-content/themes/responsive/inc/customizer.php
@@ -36,5 +36,61 @@ function lol_widgets_init(){
 			'after_title' => '</h3>',
 		));
 	}
+	register_widget('Download_Widget');
 }
 add_action( 'widgets_init', 'lol_widgets_init' );
+
+
+/* Tải Button */
+class Download_Widget extends WP_Widget
+{
+	function Download_Widget()
+	{
+		$widget_ops = array('classname' => 'DownloadWidget', 'description' => 'Displays a button download' );
+		$this->WP_Widget('DownloadWidget', 'Button Download', $widget_ops);
+	}
+
+	function form($instance)
+	{
+		$instance = wp_parse_args( (array) $instance, array( 'link-android' => '' ,'link-iosj' => '' ,'link-iosb' => '' ) );
+		$linkAndroid = $instance['link-android'];
+		$linkIosJ = $instance['link-iosj'];
+		$linkIosB = $instance['link-iosb'];
+		?>
+		<p><label for="<?php echo $this->get_field_id('link-android'); ?>">Link Android: <input class="widefat" id="<?php echo $this->get_field_id('link-android'); ?>" name="<?php echo $this->get_field_name('link-android'); ?>" type="text" value="<?php echo $linkAndroid; ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('link-iosj'); ?>">Link IOS Jailbreak: <input class="widefat" id="<?php echo $this->get_field_id('link-iosj'); ?>" name="<?php echo $this->get_field_name('link-iosj'); ?>" type="text" value="<?php echo $linkIosJ; ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('link-iosb'); ?>">Link IOS Itunes: <input class="widefat" id="<?php echo $this->get_field_id('link-iosb'); ?>" name="<?php echo $this->get_field_name('link-iosb'); ?>" type="text" value="<?php echo $linkIosB; ?>" /></label></p>
+	<?php
+	}
+
+	function update($new_instance, $old_instance)
+	{
+		$instance = $old_instance;
+		$instance['link-android'] = $new_instance['link-android'];
+		$instance['link-iosj'] = $new_instance['link-iosj'];
+		$instance['link-iosb'] = $new_instance['link-iosb'];
+		return $instance;
+	}
+
+	function widget($args, $instance)
+	{
+		extract($args, EXTR_SKIP);
+
+		echo $before_widget;
+		$linkAndroid = empty($instance['link-android']) ? ' ' : $instance['link-android'];
+		$linkIosJ = empty($instance['link-iosj']) ? ' ' : $instance['link-iosj'];
+		$linkIosB = empty($instance['link-iosb']) ? ' ' : $instance['link-iosb'];
+		// WIDGET CODE GOES HERE
+		?>
+			<ul class="subdownload">
+				<li><a href="<?php echo $linkAndroid;  ?>" target="_blank"><img src="<?php echo get_bloginfo('template_url') ?>/images/event/download_1.jpg" alt="android"></a></li>
+				<li><a href="<?php echo $linkIosJ; ?>" title="iso"><img src="<?php echo get_bloginfo('template_url') ?>/images/event/download_2.jpg"></a></li>
+				<li><a title="IOS" href="<?php echo $linkIosB; ?>" target="_blank"><img src="<?php echo get_bloginfo('template_url') ?>/images/event/download_3.jpg"></a></li>
+			</ul>
+		<?php
+
+
+		echo $after_widget;
+	}
+
+}
--- wp-content/themes/responsive/single.php
+++ wp-content/themes/responsive/single.php
@@ -11,12 +11,8 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<div class="sub_wrap">
 				<div class="sub_left">
-					<ul class="subdownload">
-						<li><a href="#" target="_blank"><img src="<?php echo get_bloginfo('template_url') ?>/images/event/download_1.jpg" alt="android"></a></li>
-						<li><a href="javascript:void(0);" onclick="alert('a！')" title="iso"><img src="<?php echo get_bloginfo('template_url') ?>/images/event/download_2.jpg"></a></li>
-						<li><a title="IOS" href="#l" target="_blank"><img src="<?php echo get_bloginfo('template_url') ?>/images/event/download_3.jpg"></a></li>
-					</ul>
-					<dl class="adds"><dd><a><img src="<?php echo get_bloginfo('template_url') ?>/images/event/ads.png" alt=""></a></dd></dl>
+					<?php get_sidebar('left'); ?>
+					<dl class="adds"><dd><a><img src="<?php echo get_bloginfo('template_url') ?>/images/event/ads.jpg" alt=""></a></dd></dl>
 					<div id="example">
 						<div class="catechose">
 							<h1>NHÂN VẬT</h1>
