7f983ff745a25c8e9bc2b78daa6904222191916d
Hieu phan <mrphanhieuqt@gmail.com>
2014-10-22 23:40:14 +0700

    update landing mobile

--- wp-content/themes/responsive/js/lol.landingpage.js
+++ wp-content/themes/responsive/js/lol.landingpage.js
@@ -1,4 +1,21 @@
 $(document).on("ready", function() {
+	var truncate = function() {
+		$('.truncate').each(function() {
+			var size = $(this).width();
+			var moreText = '...';
+			var title = $(this).text();
+
+			$(this).html($(this).attr("title-text"));
+
+			$(this).truncate({
+				width: size,
+				token: moreText,
+				side: 'right',
+				multiline: false
+			});
+		});
+	}
+
 	var mobileResize = function() {
 		var winW = $(window).width();
 		var winH = 1798 * winW / 640;
@@ -6,7 +23,9 @@ $(document).on("ready", function() {
 			"width" : winW,
 			"height" : winH
 		});
+		truncate();
 	};
+
 	$(window).on("resize", function() {
 		var isMobileSize = !$(".mobile").is(":hidden");
 		if(isMobileSize) {
