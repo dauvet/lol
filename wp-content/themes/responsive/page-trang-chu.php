9390201db0fa0d50251cc52638700075055e23f5
mrphanhieuqt <mrphanhieuqt@gmail.com>
2014-10-30 09:22:08 +0700

    @H<: title của "Câu hỏi" và "Trailer" xích zô cho giống với title "cộng đồng" đi e [8:54:33 AM] Hiếu Phan: trên mobile hay desktop ? [8:55:55 AM] SMARTNET-H>: cả 2 đi, cho thống nhất [8:56:29 AM] SMARTNET-H>: hiện tại, trên desktop thì thấy "Trailer" và "cộng đồng" giống nhau, "câu hỏi" làm cho giống 2 thèn kia lun đi

--- wp-content/themes/responsive/page-trang-chu.php
+++ wp-content/themes/responsive/page-trang-chu.php
@@ -23,7 +23,7 @@ get_header("trang-chu"); ?>
             <div class="clear"></div>
         </div>
         <div class="main-content">
-            <h2 class="question-title">CÂU HỎI<!--a class="see-more" href="javascript:void(0);">xem thêm >></a--></h2>
+            <h2 class="question-title block-title">CÂU HỎI<!--a class="see-more" href="javascript:void(0);">xem thêm >></a--></h2>
             <div class="question-section">
                 <div class="question-wrapper">
                     <div id="question-content-1" class="question-content scrollbar-arrow">
@@ -48,7 +48,7 @@ get_header("trang-chu"); ?>
                 <a class="fancybox-modal question-ask-a" href="#question-ask-modal"><div class="question-ask-btn" ></div></a>
             </div>
             <div class="clip-1">
-                <h2 class="clip-title">TRAILER</h2>
+                <h2 class="clip-title block-title">TRAILER</h2>
                 <div class="clip-section">
                     <iframe width="100%" height="100%" src="<?php echo landing_settings_get("home_trailer_youtube_link") ?>" frameborder="0" allowfullscreen></iframe>
                 </div>
@@ -64,7 +64,7 @@ get_header("trang-chu"); ?>
                 ?>
             </div>
             <div class="facebook-1">
-                <h2 class="facebook-1-title">CỘNG ĐỒNG</h2>
+                <h2 class="facebook-1-title block-title">CỘNG ĐỒNG</h2>
                 <iframe id="facebook-1-desktop" class="desktop" src="//www.facebook.com/plugins/likebox.php?href=<?php echo landing_settings_get("home_facebook_link") ?>&amp;width=392&amp;height=258&amp;colorscheme=dark&amp;show_faces=true&amp;header=false&amp;stream=false&amp;show_border=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width: 392px; height:210px;" allowTransparency="true"></iframe>
                 <iframe id="facebook-1-mobile" class="mobile" src="//www.facebook.com/plugins/likebox.php?href=<?php echo landing_settings_get("home_facebook_link") ?>&amp;colorscheme=dark&amp;show_faces=true&amp;header=false&amp;stream=false&amp;show_border=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width: 100%; allowTransparency="true"></iframe>
             </div>
